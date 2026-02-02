49f20b672da997ed67d0c7f26604e8ae5dbca85f
tanishk-lodh <kanamisty2712@gmail.com>
2026-02-02 10:43:46 +0530

    Add files via upload

---
+++ IO.py
@@ -0,0 +1,23 @@
+#1 int example
+"""a = int(input("Enter your number : "))
+print(a)
+b =int(input("Enter your number : "))
+print(b)"""
+
+#2 float example
+"""Price = float(input("Enter your price"))
+print(Price)
+Tender = float(input("Enter your tender"))
+print(Tender)"""
+
+#3 str example
+"""x = "Hello atmiya"
+print(x)
+y ="Hello students"
+print(y)"""
+
+#4 boolean example
+"""Q = bool(input("True"))
+print(Q)
+Q = bool(input("Fakse"))
+print(Q)"""
